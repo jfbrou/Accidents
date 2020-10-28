a2370aba721c7d313ef89ee39bf06305d742f7e1
Jean-Félix Brouillette <jfbrou@users.noreply.github.com>
2020-10-27 20:34:19 -0400

    Update Process.py
    Create a separate script for the model section.

--- Process.py
+++ Process.py
@@ -16,10 +16,6 @@ from pvlib import solarposition
 import os
 import urllib.request as urllib
 import datetime
-from keras.layers import Input, Dense, Dropout, BatchNormalization
-from keras.models import Sequential
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 # Find the current working directory
 path = os.getcwd()
@@ -380,54 +376,11 @@ df.loc[:, 'elevation'] = dfsolar.apparent_elevation.values
 df.loc[:, 'zenith'] = dfsolar.apparent_zenith.values
 df.loc[:, 'azimuth'] = dfsolar.azimuth.values
 
+################################################################################
+#                                                                              #
+# This section of the script saves the data frame.                             #
+#                                                                              #
+################################################################################
+
 # Save the data
 df.to_csv(os.path.join(data, 'data.csv'), index=False)
-
-################################################################################
-#                                                                              #
-# This section of the script splits our data into training and test sets.      #
-#                                                                              #
-################################################################################
-
-# Keep certain columns
-df = df.dropna()
-X = df.loc[:, df.columns[~df.columns.isin(['datetime', 'segmentid', 'geometry', 'accident'])]]
-Y = df.loc[:, 'accident']
-
-# Split the data into training and test sets
-train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.05, random_state=0, shuffle=True, stratify=Y)
-
-# Cast to numpy arrays
-test_Y = np.array(test_Y)
-test_X = np.array(test_X)
-
-# Normalize the data
-scaler = StandardScaler()
-train_XN = scaler.fit_transform(train_X)
-test_XN = scaler.transform(test_X)
-
-################################################################################
-#                                                                              #
-# This section of the script defines, compiles and trains the model.           #
-#                                                                              #
-################################################################################
-
-# Define the model instance
-model = Sequential()
-model.add(Dense(128, input_dim=train_XN.shape[1], activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
-
-# Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Fit the model
-history = model.fit(x=train_XN, y=train_Y, epochs=10, batch_size=128)
-
-# Evaluate the model
-predictions = model.evaluate(x=test_XN, y=test_Y)
